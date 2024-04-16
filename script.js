55476ecc8fd2bc1249aadba230dd3d628e31e282
beatrizac03 <beatrizaguiarc14@gmail.com>
2024-04-16 10:29:09 -0300

    updt 1.5

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ import questions from './questions-answers.js'
 let questionDiv = document.querySelector('.question')
 let eachAnswer = document.querySelectorAll('.each-answer')
 let btnNext = document.querySelector('.btn-next')
+let btnStartGame = document.querySelector('.btn-startgame')
+let divContQuiz = document.querySelector('.cont-quiz')
 
 let currentIndex = 0;
 let score = 0;
+
+btnStartGame.addEventListener('click', startGame)
+
+eachAnswer.forEach( answer => {
+    answer.addEventListener('click', selectAnswer)
+})
+
+btnNext.addEventListener('click', loadQuestion)
+
 
 // 1. cada .question deve receber uma question
 // 2 . cada div .each-answer deve receber uma option
 // 3. a cada clique em .each-answer, deverá ser testado se a opção é certa.
 
+function startGame() {
+    divContQuiz.classList.remove('hidden')
+    loadQuestion()
+}
+
 function loadQuestion() {
-    questionDiv.innerHTML = questions[currentIndex].question
+    resetState()
+
+    questionDiv.textContent = questions[currentIndex].question
     questionDiv.style.color = 'white'
 
     questions[currentIndex].answers.forEach( (answer, index) => {
@@ -21,57 +39,48 @@ function loadQuestion() {
         if (answer.correct) {
             eachAnswer[index].dataset.correct = true;
         }
-})
+    })
 
+}
+
+function resetState() {
+
+    document.body.removeAttribute('class')
+    btnNext.classList.add('hidden')
+
+    eachAnswer.forEach(button => {
+        button.classList.remove('correct', 'incorrect');
+        button.disabled = false;
+    });
+
+    btnNext.classList.add('hidden');
 }
 
 if(questions[0].answers[0].correct == true) {
     console.log('sim')
 }
 
-// function chooseAnswer() {
-//     questions[currentIndex].answers.forEach( (answer, index) => {
-//         if(answer.correct) {
-//             eachAnswer[index].style.backgroundColor = 'red'
-//             eachAnswer[index].dataset.correct = answer.correct
-//         } else {
-//             eachAnswer[index].style.backgroundColor = 'green'
-//         }
-//     })
-// }
-
-
-function chooseAnswer (event) {
-    let divEv = event.target
-    console.log(divEv)
-    if (divEv.dataset.correct === "true") {
-        divEv.style.backgroundColor = 'green';
+function selectAnswer (event) {
+    let answerClicked = event.target
+    console.log(answerClicked)
+    if (answerClicked.dataset.correct) {
+        document.body.classList.add('correct')
     } else {
-        divEv.style.backgroundColor = 'red';
+        document.body.classList.add('incorrect')
     }
 
-    
-}
+    eachAnswer.forEach(button => {
+        if(button.dataset.correct) {
+            button.classList.add('correct')
+        } else {
+            button.classList.add('incorrect')
+        }
 
-function selectAnswer(event) {
-    let chosenAnswer = event.target
-    const isCorrect = chosenAnswer.dataset.correct == "true"
-    if(isCorrect){
-        chosenAnswer.classList.add("correct")
-    } else {
-        chosenAnswer.classList.add("incorrect")
-    }
-
-}
-
-eachAnswer.forEach( answer => {
-    answer.addEventListener('click', chooseAnswer)
-})
-
-btnNext.addEventListener('click', function() {
+        button.disabled = true
+    })
     currentIndex++
-    loadQuestion()
-})
+
+    btnNext.classList.remove('hidden')
+}
 
 
-loadQuestion()
